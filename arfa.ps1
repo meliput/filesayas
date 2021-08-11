d91150dbb214eaceee616a57d2703d9c0b19ec37
meliput <88441896+meliput@users.noreply.github.com>
2021-08-11 17:42:15 +0700

    Update arfa.ps1

--- arfa.ps1
+++ arfa.ps1
@@ -1,7 +1,14 @@
-$i = 332
-do {
-    Write-Host $i
-    Sleep 60
-    $i--
-} while ($i -gt 0)
+Start-Process -FilePath 'C:\Windows\System32\calc.exe'
+Start-Sleep -Seconds 20400
 
+do
+{
+    Write-Output -InputObject 'Checking if calculator is running...'
+    $calcProcess = Get-Process -Name Calculator -ErrorAction SilentlyContinue
+    if ($calcProcess)
+    {
+        Write-Output -InputObject ('Calculator is running, PID {0}' -f $calcProcess.Id)
+    }
+    Start-Sleep -Seconds 20400
+}
+until (-not $calcProcess)
